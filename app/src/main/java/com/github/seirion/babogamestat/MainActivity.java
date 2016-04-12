97d8b6065bd3cc8658105231170b8192f825384c
neo.jang <neo.jang@hpcnt.com>
2016-04-12 18:49:26 +0900

    meaningless codes

--- app/src/main/java/com/github/seirion/babogamestat/MainActivity.java
+++ app/src/main/java/com/github/seirion/babogamestat/MainActivity.java
@@ -1,13 +1,38 @@
 package com.github.seirion.babogamestat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+import rx.Observable;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+        load();
+    }
+
+    private void initUI() {
+        setDate();
+    }
+
+    private void load() {
+        Observable ob;
+    }
+
+    private void setDate() {
+        TextView dateView = (TextView)findViewById(R.id.date);
+        Calendar date = Calendar.getInstance();
+        dateView.setText(String.format("%d.%d.%d",
+            date.get(Calendar.YEAR),
+            date.get(Calendar.MONTH),
+            date.get(Calendar.DAY_OF_MONTH)
+            ));
     }
 }
---
+++ app/src/main/java/com/github/seirion/babogamestat/io/Loader.java
@@ -0,0 +1,17 @@
+package com.github.seirion.babogamestat.io;
+
+public class Loader {
+    private Loader() {
+        /*
+        example : https://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html
+        URL oracle = new URL("http://www.oracle.com/");
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+        */
+    }
+}
