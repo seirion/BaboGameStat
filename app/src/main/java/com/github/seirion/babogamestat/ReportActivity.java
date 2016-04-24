21a428a54d03afea9e083ad4fd942d6275d70b71
seirion <seirion@mensakorea.org>
2016-04-24 22:15:07 +0900

    add average and accumulate rate

--- app/src/main/java/com/github/seirion/babogamestat/ReportActivity.java
+++ app/src/main/java/com/github/seirion/babogamestat/ReportActivity.java
@@ -11,6 +11,7 @@ import java.text.DecimalFormat;
 
 public class ReportActivity extends Activity {
     private static final String TAG = ReportActivity.class.getSimpleName();
+    final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.##");
 
     private int index;
 
@@ -51,17 +52,48 @@ public class ReportActivity extends Activity {
     }
 
     private void setReport(BaboData data) {
-        final DecimalFormat decimalFormat = new DecimalFormat("#.##");
         TextView reportView = (TextView) findViewById(R.id.reportText);
         StringBuilder sb = new StringBuilder();
         float rateOfProfit = ((float)data.getCurrent() - data.getBase()) * 100 / data.getBase();
-        sb.append("누적 수익률 : " + decimalFormat.format(rateOfProfit) + " %\n");
+        sb.append("누적 : " + DECIMAL_FORMAT.format(rateOfProfit) + " %\n");
 
         if (index != 0) {
             BaboData prev = DataSet.instance().get(index - 1);
             float rate = ((float)data.getCurrent() - prev.getCurrent()) * 100 / prev.getCurrent();
-            sb.append("당일 수익률 : " + decimalFormat.format(rate) + " %\n");
+            sb.append("당일 : " + DECIMAL_FORMAT.format(rate) + " %\n");
+        }
+
+        if (20 <= DataSet.instance().size()) {
+            sb.append("20일 누적 : " + accumulateRateOf(20) + " %\n");
+            sb.append("20일 평균 : " + averageRateOf(20) + " %\n");
         }
         reportView.setText(sb.toString());
+    }
+
+    private String averageRateOf(int days) {
+        float base = 0f;
+        float current = 0f;
+        while (0 <= --days) {
+            BaboData data = DataSet.instance().get(index - days);
+            base += data.getBase();
+            current = data.getCurrent();
+        }
+
+        base /= days;
+        current /= days;
+        return DECIMAL_FORMAT.format((current - base) * 100 / base);
+    }
+
+    private String accumulateRateOf(int days) {
+        float base = 0f;
+        float current = 0f;
+        while (0 <= --days) {
+            BaboData data = DataSet.instance().get(index - days);
+            base += data.getBase();
+            current = (data.getCurrent() - data.getBase());
+        }
+
+        base /= days;
+        return DECIMAL_FORMAT.format((current - base) * 100 / base);
     }
 }
